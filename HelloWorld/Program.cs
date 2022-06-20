0b2a4da0ec5118cdb48e37bb5816c34964ff3135
Colton Smith <coltonls24@gmail.com>
2022-06-19 22:22:04 -0500

    5-line output

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("This is weird");
+Console.WriteLine("I have to write 5 lines");
+Console.WriteLine("Not 4");
+Console.WriteLine("Not 6");
+Console.WriteLine("5 simple lines");
+
